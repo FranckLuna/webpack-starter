73b2c349179e1e31ff71c47492b80004712f08f3
Franco <franco7luna@gmail.com>
2025-04-06 22:22:15 -0300

    Creando pagina de tareas1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-import { saludar } from './js/componentes'
 import './style.css';
+import {Todo, TodoList} from './clases';
+import { crearTodoHtml } from './js/componentes';
 
-const nombre = 'Franco Daniel Luna';
 
-saludar(nombre);
+export const todoList = new TodoList();
+
+const tarea = new Todo('Aprender javaScript !!!!');
+
+todoList.nuevoTodo( tarea );
+
+console.log( todoList );
+
+crearTodoHtml(tarea);
--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,11 +1,41 @@
-import '../css/componentes.css';
+import {Todo, TodoList} from '../clases';
+import { todoList } from '../index';
 
 
-export const saludar = ( nombre ) =>{
-    console.log('Creando etiqueta h1');
+// referencias en el html
+const divTodoList = document.querySelector('.todo-list');
+const txtInput = document.querySelector('.new-todo');
 
-    const h1 = document.createElement('h1');
-    h1.innerText = `hola, ${nombre} como estas`;
+
+export const crearTodoHtml = ( todo ) =>{
+    const htmlTodo = `
+    <li class="${ (todo.completado) ? 'completed' : '' }" data-id="abc">
+			<div class="view">
+				<input class="toggle" type="checkbox" ${ (todo.completado) ? 'checked' : '' }>
+				<label> ${ todo.tarea } </label>
+				<button class="destroy"></button>
+			</div>
+		<input class="edit" value="Create a TodoMVC template">
+	</li>`
+
+    const div = document.createElement('div');
+    div.innerHTML = htmlTodo;
+    divTodoList.append( div.firstElementChild );
     
-    document.body.append( h1 );
+    return div.firstElementChild;
 }
+//eventos 
+
+txtInput.addEventListener('keyup', (event) => {
+
+    if ( event.keyCode === 13 && txtInput.value.length > 0 ) {
+        //console.log( txtInput.value );
+        
+        const nuevoTodo = new Todo( txtInput.value );
+        todoList.nuevoTodo( nuevoTodo );
+
+        crearTodoHtml( nuevoTodo );
+        txtInput.value = '';
+    }
+
+});
